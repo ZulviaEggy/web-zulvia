87c6e5770e50824da1b7a5f2e735090767d3e4c1
zulviaeggy <zulviaeggy97@gmail.com>
2019-04-10 21:34:24 +0700

    Updates

---
+++ precache-manifest.3aed9917c4340931c35b349b3e273315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a44d71b44db176a27ad1a9378600f04",
+    "url": "/web-zulvia/static/media/asia.8a44d71b.svg"
+  },
+  {
+    "revision": "4f4b3a9410450dc5bf71",
+    "url": "/web-zulvia/static/js/runtime~main.4f4b3a94.js"
+  },
+  {
+    "revision": "4f1af59881ccc8190c9c",
+    "url": "/web-zulvia/static/js/main.4f1af598.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/web-zulvia/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "4f1af59881ccc8190c9c",
+    "url": "/web-zulvia/static/css/main.02af81f8.chunk.css"
+  },
+  {
+    "revision": "7aa39dfdb155de48f387c612c33a606f",
+    "url": "/web-zulvia/index.html"
+  }
+];
